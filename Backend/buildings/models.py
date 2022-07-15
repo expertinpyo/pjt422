33c369030dc74e5f21597524aaf90a67973ba2b2
장세진 <jsejin95@gmail.com>
2022-07-15 18:00:59 +0900

    backend:update model

--- Backend/buildings/models.py
+++ Backend/buildings/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from accounts.models import SchoolPeople
 
 # Create your models here.
 class Building(models.Model):
@@ -17,4 +19,10 @@ class Trashbin(models.Model):
     location_x = models.FloatField()
     location_y = models.FloatField()
     trashbin_floor = models.ForeignKey(Floor, related_name='floor_trashbin', on_delete=models.CASCADE)
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 관리자와 쓰레기통은 1:N관계
 
+
+class Trash(models.Model):
+    trashbin = models.ForeignKey(Trashbin, on_delete=models.CASCADE)
+    discard_date = models.DateTimeField(add_now_add=True)
+    people = models.ForeignKey(SchoolPeople, on_delete=models.CASCADE)
