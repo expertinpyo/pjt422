ef2a902eebf75fd70b89b4a97d3ff814a7557a06
tokataka <skygazer227@gmail.com>
2022-07-29 08:46:56 +0900

    Frontend: Add axios plugin to use single instance

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "./plugins/axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(axios).mount("#app");
