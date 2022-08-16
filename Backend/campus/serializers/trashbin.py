c1d3062fec6a4221fade627d4c3344bceebd30a4
장세진 <jsejin95@gmail.com>
2022-08-16 13:50:33 +0900

    backend:create record api

--- Backend/campus/serializers/trashbin.py
+++ Backend/campus/serializers/trashbin.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Building, Floor, Student, Trashbin, Group
+from ..models import Building, Floor, Student, Trashbin, Group, CleanRecord
 from django.contrib.auth import get_user_model
 
 # 해당 층에 쓰레기통 추가
@@ -54,3 +54,69 @@ class TrashbinNotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trashbin
         fields = '__all__'
+
+
+
+# 비움 이력 조회
+class CleanRecordSerializer(serializers.ModelSerializer):
+
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = get_user_model()
+            fields = ('pk', 'username', 'rfid_num', 'name', 'phone', 'position', )
+    
+    user = UserSerializer(read_only=True)
+    
+    class TrashbinSerializer(serializers.ModelSerializer):
+        
+        class GroupSerializer(serializers.ModelSerializer):
+
+            class FloorSerializer(serializers.ModelSerializer):
+                
+                class BuildingSerializer(serializers.ModelSerializer):
+
+                    class Meta:
+                        model = Building
+                        fields = ('pk', 'name',)
+                    
+                building = BuildingSerializer(read_only=True)
+
+                class Meta:
+                    model = Floor
+                    fields = ('pk', 'name', 'building',)
+            
+            floor = FloorSerializer(read_only=True)
+
+            class Meta:
+                model = Group
+                fields = ('pk', 'name', 'floor',)
+        
+        group = GroupSerializer(read_only=True)
+
+        class Meta:
+            model = Trashbin
+            fields = ('trash_type', 'token', 'group', )
+    
+    trashbin = TrashbinSerializer(read_only=True)
+    updated_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+
+    class Meta:
+        model = CleanRecord
+        fields = '__all__'
+
+
+
+# class CleanRecordSerializer(serializers.ModelSerializer):
+    
+#     class UserSerializer(serializers.ModelSerializer):
+#         class Meta:
+#             model = get_user_model()
+#             fields = ('pk', 'username', 'rfid_num', 'name', 'phone', 'position', )
+
+#     user = UserSerializer(read_only=True)  
+
+#     trashbin = TrashbinNotificationSerializer(read_only=True)
+
+#     class Meta:
+#         model = CleanRecord
+#         fields = '__all__'
--- Backend/campus/urls.py
+++ Backend/campus/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('notification/', NotificationView.as_view()), # 쓰레기통 알림
     path('logdata/<rfid>/<int:trashbin_pk>/', LogView.as_view()), # 로그 데이터 조회
     path('test/', TrashView.as_view()), # 로그 데이터 조회
+    path('trashbin/record/', RecordView.as_view()),
     
     #path('all/<str:rfid>/', views.check_all),  # 신원 조회
     #path('state/<str:token>/', views.trashbin_type),  # 쓰레기통 종류 조회
